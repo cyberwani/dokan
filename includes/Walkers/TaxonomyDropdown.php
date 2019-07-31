5ae531f9b311b199a7f7f9d5c7e515293cbde313
Edi Amin <to.ediamin@gmail.com>
2019-07-31 12:06:15 +0600

    refactor: Rebase refactor branch with develop branch

---
+++ includes/Walkers/TaxonomyDropdown.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace WeDevs\Dokan\Walkers;
+
+use Walker;
+
+class TaxonomyDropdown extends Walker {
+
+    /**
+     * @see Walker::$tree_type
+     * @var string
+     */
+    public $tree_type = 'category';
+
+    /**
+     * @see Walker::$db_fields
+     * @var array
+     */
+    public $db_fields = array('parent' => 'parent', 'id' => 'term_id');
+
+    private $post_id = '';
+
+    public function __construct( $post_id = '' )  {
+        $this->post_id = $post_id;
+    }
+
+    public function start_el( &$output, $category, $depth = 0, $args = array(), $id = 0 ) {
+        $commission_val  = dokan_get_seller_percentage( dokan_get_current_user_id(), $this->post_id, $category->term_id );
+        $commission_type = dokan_get_commission_type( dokan_get_current_user_id(), $this->post_id, $category->term_id );
+
+        $pad      = str_repeat( '&nbsp;', $depth * 3 );
+        $cat_name = apply_filters( 'list_cats', $category->name, $category );
+
+        $output .= "\<option class=\"level-$depth\" value=\"" . $category->term_id . "\"";
+        $output .= ' data-commission="' . $commission_val . '" data-commission_type="' . $commission_type . '"';
+
+        if ( in_array( $category->term_id, $args['selected'] ) ) {
+            $output .= ' selected="selected"';
+        }
+
+        $output .= '>';
+        $output .= $pad . $cat_name;
+
+        if ( $args['show_count'] ) {
+            $output .= '&nbsp;&nbsp;(' . $category->count . ')';
+        }
+
+        $output .= "</option>\n";
+    }
+}
